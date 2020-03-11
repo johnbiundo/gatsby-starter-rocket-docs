96a13e897331087e65a44b422651bab80de1bbd4
john <johnfbiundo@gmail.com>
2020-03-11 16:57:21 -0700

    Initial commit from gatsby: (git@github.com:johnbiundo/gatsby-starter-rocket-docs.git)

---
+++ gatsby-config.js
@@ -0,0 +1,48 @@
+module.exports = {
+  siteMetadata: {
+    siteTitle: `John Site`,
+    defaultTitle: `John Site`,
+    siteTitleShort: `John Site`,
+    siteDescription: `John Site`,
+    siteUrl: `https://rocketdocs.netlify.com`,
+    siteAuthor: `John Site`,
+    siteImage: `/banner.png`,
+    siteLanguage: `en`,
+    themeColor: `#7159c1`,
+    footer: `John Site`,
+  },
+  plugins: [
+    {
+      resolve: `@rocketseat/gatsby-theme-docs`,
+      options: {
+        configPath: `src/config`,
+        docsPath: `src/docs`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Rocketseat Gatsby Themes`,
+        short_name: `RS Gatsby Themes`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        display: `standalone`,
+        icon: `static/favicon.png`,
+      },
+    },
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `UA-99997611-9`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `https://rocketdocs.netlify.com`,
+      },
+    },
+    `gatsby-plugin-offline`,
+  ],
+};
